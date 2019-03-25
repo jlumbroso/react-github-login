ac120da34ff90b961f6a2f1110839c667b586add
manishsaraan <manish021js@gmail.com>
2019-03-25 15:20:57 +0530

    Readme updated and minor code refactor

--- src/GitHubLogin.js
+++ src/GitHubLogin.js
@@ -1,66 +1,59 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import { func, node, string } from "prop-types";
 
-import PopupWindow from './PopupWindow';
-import { toQuery } from './utils';
+import PopupWindow from "./PopupWindow";
+import { toQuery } from "./utils";
 
 class GitHubLogin extends Component {
   static propTypes = {
-    buttonText: PropTypes.string,
-    children: PropTypes.node,
-    className: PropTypes.string,
-    clientId: PropTypes.string.isRequired,
-    onRequest: PropTypes.func,
-    onSuccess: PropTypes.func,
-    onFailure: PropTypes.func,
-    redirectUri: PropTypes.string,
-    scope: PropTypes.string,
-  }
+    buttonText: string,
+    children: node,
+    className: string,
+    clientId: string.isRequired,
+    onRequest: func,
+    onSuccess: func,
+    onFailure: func,
+    redirectUri: string,
+    scope: string
+  };
 
   static defaultProps = {
-    buttonText: 'Sign in with GitHub',
-    redirectUri: '',
-    scope: 'user:email',
+    buttonText: "Sign in with GitHub",
+    redirectUri: "",
+    scope: "user:email",
     onRequest: () => {},
     onSuccess: () => {},
-    onFailure: () => {},
-  }
+    onFailure: () => {}
+  };
 
   onBtnClick = () => {
     const { clientId, scope, redirectUri } = this.props;
     const search = toQuery({
       client_id: clientId,
       scope,
-      redirect_uri: redirectUri,
+      redirect_uri: redirectUri
     });
-    const popup = this.popup = PopupWindow.open(
-      'github-oauth-authorize',
+    const popup = (this.popup = PopupWindow.open(
+      "github-oauth-authorize",
       `https://github.com/login/oauth/authorize?${search}`,
       { height: 1000, width: 600 }
-    );
+    ));
 
     this.onRequest();
-    popup.then(
-      data => this.onSuccess(data),
-      error => this.onFailure(error)
-    );
-  }
+    popup.then(data => this.onSuccess(data), error => this.onFailure(error));
+  };
 
-  onRequest = () => {
-    this.props.onRequest();
-  }
+  onRequest = () => this.props.onRequest();
 
-  onSuccess = (data) => {
+  onSuccess = data => {
     if (!data.code) {
-      return this.onFailure(new Error('\'code\' not found'));
+      return this.onFailure(new Error("'code' not found"));
     }
 
     this.props.onSuccess(data);
-  }
+  };
 
-  onFailure = (error) => {
-    this.props.onFailure(error);
-  }
+  onFailure = error => this.props.onFailure(error);
 
   render() {
     const { className, buttonText, children } = this.props;
@@ -70,7 +63,7 @@ class GitHubLogin extends Component {
       attrs.className = className;
     }
 
-    return <button {...attrs}>{ children || buttonText }</button>;
+    return <button {...attrs}>{children || buttonText}</button>;
   }
 }
 
